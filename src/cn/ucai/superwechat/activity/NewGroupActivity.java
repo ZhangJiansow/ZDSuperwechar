05fa6bc57aec8813d2a98cb37e53669ba9372878
ZhangJiansow <1498072529@qq.com>
2016-07-28 15:09:08 +0800

    新建公开群保存到全局变量中

--- src/cn/ucai/superwechat/activity/NewGroupActivity.java
+++ src/cn/ucai/superwechat/activity/NewGroupActivity.java
@@ -185,15 +185,9 @@ public class NewGroupActivity extends BaseActivity {
 						Log.e(TAG, "result=" + result);
 						if (result != null && result.isRetMsg()) {
 							if (members != null && members.length > 0) {
-								addGroupMembers(groupId,members);
+								addGroupMembers(groupId,members,groupAvatar);
 							} else {
-								runOnUiThread(new Runnable() {
-									public void run() {
-										progressDialog.dismiss();
-										setResult(RESULT_OK);
-										finish();
-									}
-								});
+								createGroupSuccess(groupAvatar);
 							}
 						}
 					}
@@ -208,7 +202,7 @@ public class NewGroupActivity extends BaseActivity {
 
 	}
 
-	private void addGroupMembers(String hxid, String[] members) {
+	private void addGroupMembers(String hxid, String[] members,final GroupAvatar group) {
 		Log.e(TAG, "members=" + members);
 		Log.e(TAG, "members=" + members.toString());
 		String memberArr = "";
@@ -230,13 +224,7 @@ public class NewGroupActivity extends BaseActivity {
 						GroupAvatar groupAvatar = (GroupAvatar) result.getRetData();
 						Log.e(TAG, "result=" + result);
 						if (result != null && result.isRetMsg()) {
-							runOnUiThread(new Runnable() {
-								public void run() {
-									progressDialog.dismiss();
-									setResult(RESULT_OK);
-									finish();
-								}
-							});
+							createGroupSuccess(group);
 						} else {
 							progressDialog.show();
 							Toast.makeText(NewGroupActivity.this, st2, Toast.LENGTH_LONG).show();
@@ -250,6 +238,18 @@ public class NewGroupActivity extends BaseActivity {
 						Toast.makeText(NewGroupActivity.this, st2 + error, Toast.LENGTH_LONG).show();
 					}
 				});
+	}
+
+	private void createGroupSuccess(GroupAvatar group) {
+		SuperWeChatApplication.getInstance().getGroupMap().put(group.getMGroupHxid(), group);
+		SuperWeChatApplication.getInstance().getGroupList().add(group);
+		runOnUiThread(new Runnable() {
+			public void run() {
+				progressDialog.dismiss();
+				setResult(RESULT_OK);
+				finish();
+			}
+		});
 	}
 
 	private void setProgressDialog() {
