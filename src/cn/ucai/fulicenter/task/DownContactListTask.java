c8758b3ee036e16059b902a3849a88671beb3ca9
ZhangJiansow <1498072529@qq.com>
2016-07-29 11:42:35 +0800

    init fulicenter code

---
+++ src/cn/ucai/fulicenter/task/DownContactListTask.java
@@ -0,0 +1,60 @@
+package cn.ucai.fulicenter.task;
+
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+import java.util.List;
+import java.util.Map;
+
+import cn.ucai.fulicenter.I;
+import cn.ucai.fulicenter.SuperWeChatApplication;
+import cn.ucai.fulicenter.bean.Result;
+import cn.ucai.fulicenter.bean.UserAvatar;
+import cn.ucai.fulicenter.data.OkHttpUtils2;
+import cn.ucai.fulicenter.utils.Utils;
+
+/**
+ * Created by sks on 2016/7/21.
+ */
+public class DownContactListTask {
+    private final static String TAG = DownContactListTask.class.getSimpleName();
+    String username;
+    Context mContext;
+
+
+    public DownContactListTask(String username, Context context) {
+        mContext = context;
+        this.username = username;
+    }
+
+    public void execute() {
+        OkHttpUtils2<String> utils2 = new OkHttpUtils2<>();
+        utils2.setRequestUrl(I.REQUEST_DOWNLOAD_CONTACT_ALL_LIST)
+                .addParam(I.Contact.USER_NAME,username)
+                .targetClass(String.class)
+                .execute(new OkHttpUtils2.OnCompleteListener<String>() {
+                    @Override
+                    public void onSuccess(String s) {
+
+                        Log.e(TAG, "s" + s);
+                        Result result = Utils.getListResultFromJson(s, UserAvatar.class);
+
+                        List<UserAvatar> list = (List<UserAvatar>) result.getRetData();
+                        if ((list != null) && (list.size() > 0)) {
+                            SuperWeChatApplication.getInstance().setUserList(list);
+                            mContext.sendStickyBroadcast(new Intent("update_contact_list"));
+                            Map<String, UserAvatar> userMap = SuperWeChatApplication.getInstance().getUserMap();
+                            for (UserAvatar u : list) {
+                                userMap.put(u.getMUserName(),u);
+                            }
+                        }
+                    }
+
+                    @Override
+                    public void onError(String error) {
+                        Log.e(TAG, "error" + error);
+                    }
+                });
+    }
+}
